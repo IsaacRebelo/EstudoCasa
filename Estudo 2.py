a01a6c85c68930e8c83bf28288630c6304086871
Isaac <isaacaraujorebelo@enta.pt>
2022-09-27 22:07:54 +0000

    exercicios de aprendizagem

---
+++ Estudo 2.py
@@ -0,0 +1,16 @@
+
+
+km = float(input('introduza a distância percorrida'))
+m = float(input('introduza o tempo que demorou em minutos'))
+
+horas = m/60
+
+segundos = m*60
+
+speed = km / horas
+
+metros = km *1000
+
+Velocidade_media = metros / segundos
+
+print(f'{speed} and {Velocidade_media}')
---
+++ estudo.py
@@ -0,0 +1,17 @@
+"programa que diga o ano em que nasceu"
+
+
+Idade=int(input('Insira a sua idade'))
+
+Ano_Actual =int(2020)
+
+Ano_De_Nascimento= Ano_Actual - Idade
+
+print(f'O seu ano de nascimento é {Ano_De_Nascimento}')
+
+
+x = int(input('introduza um numero'))
+y = int(input('introduza um numero'))
+
+conta = (x + 3 * y) * (x -y)
+print(conta)
